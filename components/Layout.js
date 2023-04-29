dbe87ab31df77e61b598db74eb765086748d5093
atroopa <atroopa@gmail.com>
2023-04-29 19:36:18 +0330

    layout work true

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,10 @@
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export default function Home() {
+export default function Layout({children}) {
   return (
-    <div>
-      <Head>
-        <title>Pars Click - Home</title>
-        <meta name="keyword" content="home , persian, iranian , parsclick, tutorial"/>
-      </Head>
-      <div className='text-5xl'>
-        Welcome to next
-      </div>
+    <div className="flex flex-col justify-center items-center">
+        <main className="bg-red-500">
+            {children}
+        </main>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
+//import "../styles/globals.css"
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div>
+
       <Head>
         <title>Pars Click - Home</title>
         <meta name="keyword" content="home , persian, iranian , parsclick, tutorial"/>
       </Head>
+
       <div className='text-5xl'>
         Welcome to next
       </div>
+
     </div>
   )
 }
