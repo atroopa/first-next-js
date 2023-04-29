203645b675ff41363ca68d02a06eca086aa934d1
atroopa <atroopa@gmail.com>
2023-04-29 17:40:16 +0330

    coded head and meta tag

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function about() {
+    return (
+        <div>
+            <Head>
+                <title>ParsClick - About</title>
+                <meta name="keyword" content="about, persian, iranian , parsclick, tutorial"/>
+            </Head>
+            <div className="text-5xl font-bold">about</div>
+        </div>
+    );
+}
